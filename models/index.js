e26b1394ec93b2d47067c978ae64f6ed18563a71
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-01-12 20:55:00 -0800

    initial migrating data type

--- models/index.js
+++ models/index.js
@@ -6,3 +6,44 @@ module.exports = {
 	Game: require("./Game.js")(sequelize, Model, DataTypes),
 	User: require("./User.js")(sequelize, Model, DataTypes),
 };
+
+module.exports = (sequelize, Model, DataTypes) => {
+
+	const Games = sequelize.define("games", {
+		gameName: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
+		id: 
+	});
+
+	class Game extends Model {}
+
+	Game.init(
+		{
+			id: {
+				type: DataTypes.UUID,
+				defaultValue: DataTypes.UUIDV4,
+				primaryKey: true,
+			},
+			Name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: "Uno",
+			},
+			Players: {
+				type: DataTypes.Number,
+				allowNull: false,
+				defaultValue: 1,
+			},
+			Type: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: "Card",
+			},
+		},
+		{ sequelize, modelName: "game" }
+	);
+
+	return Games;
+};
